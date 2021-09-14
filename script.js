961b7cad8a4b25e27cadf3e1d5f88d54006a64a1
Hermando Thiago <hermando2013@gmail.com>
2021-09-14 09:31:48 -0300

    :alien: final commit

--- script.js
+++ script.js
@@ -3,10 +3,15 @@
     let button = document.getElementById("generate").addEventListener("click", randomColor)
     let copiar = document.getElementById("copy").addEventListener("click", () => {
 
+        let text = document.querySelector("#hex")
+        text.select()
+        document.execCommand("copy")
+
         document.getElementById("span").innerHTML = "copied!"
 
     })  
 
+    // reset span 
     function resetMessage(){
         document.getElementById("span").innerHTML = ""
     }
@@ -18,13 +23,15 @@
         let letters = "0123456789ABCDEF"
         let hex = "#"
 
+
+        // Geração de HEX
         for(var i = 0; i < 6; i++){
             hex += letters[Math.floor(Math.random()*16)]
         }
 
-        let text = document.querySelector("h3")
+        let text = document.querySelector("#hex")
         let div = document.querySelector(".color")
         
         div.style.backgroundColor = hex
-        text.innerHTML = hex
+        text.value = hex
     }
